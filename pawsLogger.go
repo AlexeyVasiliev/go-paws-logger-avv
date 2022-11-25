0b8ca2ad79068ff4947f8f85e07e119a32345fc9
Alexey Vasiliev <a.v.vasiliev@altatec.ru>
2022-11-25 15:13:52 +0300

    init

---
+++ pawsLogger.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"time"
+
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func resolveLogLevel(levelName string) zapcore.Level {
+	switch levelName {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	case "warn":
+		return zap.WarnLevel
+	case "panic":
+		return zap.PanicLevel
+	case "off":
+		return zap.PanicLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func CreateLogger(logDir string, loggerName string, levelName string, maxAgeDays int, rotationDays int) (*zap.SugaredLogger, error) {
+	logFile := logDir + "/%Y-%m-%d/" + loggerName + ".json"
+	rotator, err := rotatelogs.New(
+		logFile,
+		rotatelogs.WithMaxAge(time.Duration(maxAgeDays)*24*time.Hour),
+		rotatelogs.WithRotationTime(time.Duration(rotationDays)*24*time.Hour))
+	if err != nil {
+		return nil, err
+	}
+
+	// add the encoder config and rotator to create a new zap logger
+	w := zapcore.AddSync(rotator)
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()),
+		w,
+		resolveLogLevel(levelName))
+	logger := zap.New(core)
+	sugar := logger.Sugar()
+	return sugar, nil
+}
